fe53ca265b8039fc0286e4d6f5d05d3206cd2945
Moriba Hebelamou <trustify@trustify>
2024-12-22 13:28:56 +0000

    clean

--- pipes/array-size.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ArraySizePipe } from './array-size.pipe';
-
-describe('ArraySizePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ArraySizePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
